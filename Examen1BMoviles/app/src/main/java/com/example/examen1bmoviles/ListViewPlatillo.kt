9a67555a44443cae250079a1609628ef0afc6756
Fernando Soto <fernando.soto@epn.edu.ec>
2023-07-11 22:51:32 -0500

    Eliminar platillo

--- Examen1BMoviles/app/src/main/java/com/example/examen1bmoviles/ListViewPlatillo.kt
+++ Examen1BMoviles/app/src/main/java/com/example/examen1bmoviles/ListViewPlatillo.kt
@@ -3,6 +3,10 @@ package com.example.examen1bmoviles
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -10,10 +14,11 @@ import android.widget.TextView
 
 class ListViewPlatillo : AppCompatActivity() {
 
+    var idItemSeleccionadoPlatillo = 0
     var restauranteId = 0
-    var idItemSeleccionado = 0
+    var idItemSeleccionadoRestaurante = 0
     val arregloPlatillo = BaseDatosMemoria.arregloPlatillo
-
+    var subArregloPlatilloXRest = ArrayList<Platillo>()
 
     companion object {
         lateinit var adaptadorPlatillo: ArrayAdapter<Platillo>
@@ -26,14 +31,15 @@ class ListViewPlatillo : AppCompatActivity() {
 
 
         // Se recupera las variables enviadas en la anterior vista
-        idItemSeleccionado = intent.getIntExtra("idItemSeleccionado", idItemSeleccionado)
+        idItemSeleccionadoRestaurante =
+            intent.getIntExtra("idItemSeleccionado", idItemSeleccionadoRestaurante)
         restauranteId = intent.getIntExtra("restauranteId", restauranteId)
 
 
         // Generamos un subarreglo de platillos que cumplan con el restaurante seleccionado
-        var subArregloPlatilloXRest = arregloPlatillo.filter { platillo ->
+        subArregloPlatilloXRest = arregloPlatillo.filter { platillo ->
             platillo.restauranteId == restauranteId
-        }
+        } as ArrayList<Platillo>
 
 
         // Inicializar adaptador
@@ -59,14 +65,15 @@ class ListViewPlatillo : AppCompatActivity() {
             irAaniadirPlatillo(CreacionPlatillo::class.java)
         }
 
+        registerForContextMenu(listViewPlatillo)
     }
 
     override fun onResume() {
         super.onResume()
 
-        var subArregloPlatilloXRest = arregloPlatillo.filter { platillo ->
+        subArregloPlatilloXRest = arregloPlatillo.filter { platillo ->
             platillo.restauranteId == restauranteId
-        }
+        } as ArrayList<Platillo>
 
 
         // Inicializar adaptador
@@ -80,13 +87,44 @@ class ListViewPlatillo : AppCompatActivity() {
         adaptadorPlatillo.notifyDataSetChanged()
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        // llenar las opciones del menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_context_platillo, menu)
 
+        // obtener el id del ArrayList seleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        idItemSeleccionadoPlatillo = id
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_editar_platillo -> {
+
+                return true
+            }
+
+            R.id.menu_eliminar_platillo -> {
+                eliminarPlatillo(adaptadorPlatillo, idItemSeleccionadoPlatillo)
+                adaptadorPlatillo.notifyDataSetChanged()
+                return true
+            }
+
+            else -> super.onContextItemSelected(item)
+        }
+    }
 
     fun irAaniadirPlatillo(
         clase: Class<*>
     ) {
         val intent = Intent(this, clase)
-        intent.putExtra("idItemSeleccionado", idItemSeleccionado)
+        intent.putExtra("idItemSeleccionado", idItemSeleccionadoRestaurante)
         intent.putExtra("restauranteId", restauranteId)
         startActivity(intent)
 
@@ -95,8 +133,16 @@ class ListViewPlatillo : AppCompatActivity() {
     fun mostrarNombreRestaurante() {
         val textViewSeleccionadoPlatillo = findViewById<TextView>(R.id.txt_nombre_res_platillo)
         textViewSeleccionadoPlatillo.text =
-            BaseDatosMemoria.arregloRestaurante[idItemSeleccionado].nombre
+            BaseDatosMemoria.arregloRestaurante[idItemSeleccionadoRestaurante].nombre
     }
 
+    fun eliminarPlatillo(
+        adaptador: ArrayAdapter<Platillo>,
+        idEliminado: Int
+    ) {
+        arregloPlatillo.removeAt(idEliminado)
+        subArregloPlatilloXRest.removeAt(idEliminado)
+        adaptador.notifyDataSetChanged()
 
+    }
 }
