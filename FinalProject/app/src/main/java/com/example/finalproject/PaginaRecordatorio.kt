f4d57be3b05b92c8603d87f025cb82cc1383ed43
Fernando Soto <fernando.soto@epn.edu.ec>
2023-09-13 23:19:00 -0500

    correciones tipeo

---
+++ FinalProject/app/src/main/java/com/example/finalproject/PaginaRecordatorio.kt
@@ -0,0 +1,28 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.adapter.ActividadAdapter
+import com.example.finalproject.adapter.RecordatorioAdapter
+import com.example.finalproject.firestore.RecyclerProvider
+
+class PaginaRecordatorio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recordatorio)
+        initRecyclerRecordatorio()
+    }
+
+    private fun initRecyclerRecordatorio() {
+        val recyclerViewRecordatorio = findViewById<RecyclerView>(R.id.rv_recordatorio)
+        recyclerViewRecordatorio.layoutManager = LinearLayoutManager(this)
+
+        val provider = RecyclerProvider()
+        provider.listRecordatorioLiveData.observe(this) { listRecordatorio ->
+            recyclerViewRecordatorio.adapter = RecordatorioAdapter(listRecordatorio)
+        }
+        provider.cargarRecordatorio()
+    }
+}
