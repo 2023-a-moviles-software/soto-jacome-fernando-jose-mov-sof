5aeb329a3a3ade416d0870e5cbd72c1697619d86
Fernando Soto <fernando.soto@epn.edu.ec>
2023-09-14 10:26:44 -0500

    adding layout user

---
+++ FinalProject/app/src/main/java/com/example/finalproject/pagina/PaginaUser.kt
@@ -0,0 +1,12 @@
+package com.example.finalproject.pagina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalproject.R
+
+class PaginaUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
